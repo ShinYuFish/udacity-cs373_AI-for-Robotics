598d5b2789911c47ede9d9f1f2eeacdf9b19a0a5
ShinYuFish <b03502010@ntu.edu.tw>
2018-08-18 11:30:33 +0800

    1.finish first_search.py

--- 4_search/first_search.py
+++ 4_search/first_search.py
@@ -21,6 +21,7 @@ grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -34,49 +35,51 @@ delta_name = ['^', '<', 'v', '>']
 
 def move(pos, delta, grid):
     new_pos = [0,0,0]
-    new_pos[0] = pos[0] + delta[0] 
-    new_pos[1] = pos[1] + delta[1]
-    new_pos[2] = pos[2] + 1
-    if new_pos[0] >=0 and new_pos[1] >= 0: # check new point inside the grid or not
-        if grid[new_pos[0]][new_pos[1]] == 0: # check new point don't touch the wall
+    new_pos[0] = pos[0] + 1 
+    new_pos[1] = pos[1] + delta[0]
+    new_pos[2] = pos[2] + delta[1]
+    if len(grid)-1 >= new_pos[1] >=0 and len(grid[0])-1>= new_pos[2] >= 0: # check new point inside the grid or not
+        if grid[new_pos[1]][new_pos[2]] == 0: # check new point don't touch the wall
             return new_pos
 
-def priority(checklist):
+def priority(openlist):
     min_value = 0;
     min_index = 0;
-    for i in range(len(checklist)):
-        if checklist[i][2] < min_value:
-            min_value = checklist[i][2]
+    for i in range(len(openlist)):
+        if openlist[i][0] < min_value:
+            min_value = openlist[i][0]
             min_index = i 
-    return checklist[min_index]
+    return openlist[min_index]
+
+def inChecklist(checklist, newpos):
+    if newpos[1:3] in checklist:
+        return True
+    else:
+        return False
 
 def search(grid,init,goal,cost):
     # ----------------------------------------
     # insert code here
     # ----------------------------------------
     cur_pos = [0, 0, 0]
-    checklist =[]
-    checklist.append(cur_pos)
-    path = 0
+    openlist =[]
+    openlist.append(cur_pos)
+    checklist = []
+    checklist.append(cur_pos[1:3])
     found = 0
-    while checklist:
+    while openlist:
         for i in range(len(delta)):
-            cur_pos = priority(checklist)
+            cur_pos = priority(openlist)
             new_pos = move(cur_pos, delta[i], grid)
-            if new_pos:
-                print "new:", new_pos
-                if new_pos[0] == goal[0] and new_pos[1] == goal[1]:
+            if new_pos and not inChecklist(checklist, new_pos):
+                checklist.append(new_pos[1:3])
+                if new_pos[1] == goal[0] and new_pos[2] == goal[1]:
                     found = 1
                     path = new_pos
-                    print "found"
-                if new_pos not in checklist:
-                    checklist.append(new_pos)
-                    print "add"
-        
-        checklist.remove(cur_pos)
-    #if found == 0:
-    #    path = "fail"
+                if new_pos not in openlist:
+                    openlist.append(new_pos)        
+        openlist.remove(cur_pos)
+    if found == 0:
+        path = "fail"
     return path
 
-path = search(grid,init,goal,cost)
-print(path)
