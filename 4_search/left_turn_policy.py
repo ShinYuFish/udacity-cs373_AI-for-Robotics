3c4e3e64b96cb629fba97939f5812f9f6b8ae93d
ShinYuFish <b03502010@ntu.edu.tw>
2018-08-24 10:18:52 +0800

    1. update left_turn_policy structure

---
+++ 4_search/left_turn_policy.py
@@ -0,0 +1,100 @@
+# ----------
+# User Instructions:
+# 
+# Implement the function optimum_policy2D below.
+#
+# You are given a car in grid with initial state
+# init. Your task is to compute and return the car's 
+# optimal path to the position specified in goal; 
+# the costs for each motion are as defined in cost.
+#
+# There are four motion directions: up, left, down, and right.
+# Increasing the index in this array corresponds to making a
+# a left turn, and decreasing the index corresponds to making a 
+# right turn.
+
+forward = [[-1,  0], # go up
+           [ 0, -1], # go left
+           [ 1,  0], # go down
+           [ 0,  1]] # go right
+forward_name = ['up', 'left', 'down', 'right']
+
+# action has 3 values: right turn, no turn, left turn
+action = [-1, 0, 1]
+action_name = ['R', '#', 'L']
+
+# EXAMPLE INPUTS:
+# grid format:
+#     0 = navigable space
+#     1 = unnavigable space 
+grid = [[1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1]]
+
+init = [4, 3, 0] # given in the form [row,col,direction]
+                 # direction = 0: up
+                 #             1: left
+                 #             2: down
+                 #             3: right
+                
+goal = [2, 0] # given in the form [row,col]
+
+cost = [2, 1, 20] # cost has 3 values, corresponding to making 
+                  # a right turn, no turn, and a left turn
+
+# EXAMPLE OUTPUT:
+# calling optimum_policy2D with the given parameters should return 
+# [[' ', ' ', ' ', 'R', '#', 'R'],
+#  [' ', ' ', ' ', '#', ' ', '#'],
+#  ['*', '#', '#', '#', '#', 'R'],
+#  [' ', ' ', ' ', '#', ' ', ' '],
+#  [' ', ' ', ' ', '#', ' ', ' ']]
+# ----------
+
+# ----------------------------------------
+# modify code below
+# ----------------------------------------
+
+def optimum_policy2D(grid,init,goal,cost):
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+  
+    x = init[0]
+    y = init[1]
+    d = init[2]
+    g = 0
+
+    open = [[g, x, y, d]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    policy2D = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return 'fail'
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            d = next[3]
+            g = next[0]
+            
+            if x == goal[0] and y == goal[1]:
+                expand[x][y] = '*'
+                found = True
+            else:
+                for i in range(len(action)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+    return policy2D
