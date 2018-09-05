aaed852ee02000ad8ec641dbde197281b30549b0
Jamie Chen <i-shinyu.chen@aeolusbot.com>
2018-09-05 10:30:03 +0800

    1. finish final_project part1 2. init part2

--- 7_final_project/2_adding_noise/studentMain.py
+++ 7_final_project/2_adding_noise/studentMain.py
@@ -1,48 +1,19 @@
 # ----------
-# Background
-# 
-# A robotics company named Trax has created a line of small self-driving robots 
-# designed to autonomously traverse desert environments in search of undiscovered
-# water deposits.
+# Part Two
 #
-# A Traxbot looks like a small tank. Each one is about half a meter long and drives
-# on two continuous metal tracks. In order to maneuver itself, a Traxbot can do one
-# of two things: it can drive in a straight line or it can turn. So to make a 
-# right turn, A Traxbot will drive forward, stop, turn 90 degrees, then continue
-# driving straight.
-#
-# This series of questions involves the recovery of a rogue Traxbot. This bot has 
-# gotten lost somewhere in the desert and is now stuck driving in an almost-circle: it has
-# been repeatedly driving forward by some step size, stopping, turning a certain 
-# amount, and repeating this process... Luckily, the Traxbot is still sending all
-# of its sensor data back to headquarters.
-#
-# In this project, we will start with a simple version of this problem and 
-# gradually add complexity. By the end, you will have a fully articulated
-# plan for recovering the lost Traxbot.
-# 
-# ----------
-# Part One
-#
-# Let's start by thinking about circular motion (well, really it's polygon motion
-# that is close to circular motion). Assume that Traxbot lives on 
-# an (x, y) coordinate plane and (for now) is sending you PERFECTLY ACCURATE sensor 
-# measurements. 
-#
-# With a few measurements you should be able to figure out the step size and the 
-# turning angle that Traxbot is moving with.
-# With these two pieces of information, you should be able to 
-# write a function that can predict Traxbot's next location.
-#
-# You can use the robot class that is already written to make your life easier. 
-# You should re-familiarize yourself with this class, since some of the details
-# have changed. 
+# Now we'll make the scenario a bit more realistic. Now Traxbot's
+# sensor measurements are a bit noisy (though its motions are still
+# completetly noise-free and it still moves in an almost-circle).
+# You'll have to write a function that takes as input the next
+# noisy (x, y) sensor measurement and outputs the best guess 
+# for the robot's next position.
 #
 # ----------
 # YOUR JOB
 #
-# Complete the estimate_next_pos function. You will probably want to use
-# the OTHER variable to keep track of information about the runaway robot.
+# Complete the function estimate_next_pos. You will be considered 
+# correct if your estimate is within 0.01 stepsizes of Traxbot's next
+# true position. 
 #
 # ----------
 # GRADING
@@ -55,38 +26,25 @@
 
 # These import steps give you access to libraries which you may (or may
 # not) want to use.
-from robot import *
+from robot import *  # Check the robot.py tab to see how this works.
 from math import *
-from matrix import *
+from matrix import * # Check the matrix.py tab to see how this works.
 import random
 
-
-# This is the function you have to write. The argument 'measurement' is a 
+# This is the function you have to write. Note that measurement is a 
 # single (x, y) point. This function will have to be called multiple
 # times before you have enough information to accurately predict the
 # next position. The OTHER variable that your function returns will be 
 # passed back to your function the next time it is called. You can use
 # this to keep track of important information over time.
-def estimate_next_pos(measurement, OTHER=None):
+def estimate_next_pos(measurement, OTHER = None):
     """Estimate the next (x, y) position of the wandering Traxbot
     based on noisy (x, y) measurements."""
 
     # You must return xy_estimate (x, y), and OTHER (even if it is None) 
     # in this order for grading purposes.
-    # OTHER = [previous_xy, heading, turning, distance]
-    
-    if not OTHER:
-        OTHER = [measurement, 0, 0, 0]    
-    
-    distance = distance_between(measurement, OTHER[0])
-    heading = atan2(measurement[1] - OTHER[0][1], measurement[0] - OTHER[0][0])
-    turning = heading - OTHER[2]
-    OTHER = [measurement, heading, turning, distance]
-
-    guess_robot = robot(measurement[0], measurement[1], heading, turning, distance)
-    guess_robot.move(guess_robot.turning, guess_robot.distance)
-    xy_estimate = (guess_robot.x, guess_robot.y)
-    return xy_estimate, OTHER 
+    xy_estimate = (3.2, 9.1)
+    return xy_estimate, OTHER
 
 # A helper function you may find useful.
 def distance_between(point1, point2):
@@ -105,7 +63,7 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
     # if you haven't localized the target bot, make a guess about the next
     # position, then we move the bot and compare your guess to the true
     # next position. When you are close enough, we stop checking.
-    while not localized and ctr <= 10: 
+    while not localized and ctr <= 1000:
         ctr += 1
         measurement = target_bot.sense()
         position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
@@ -115,7 +73,7 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
         if error <= distance_tolerance:
             print "You got it right! It took you ", ctr, " steps to localize."
             localized = True
-        if ctr == 10:
+        if ctr == 1000:
             print "Sorry, it took you too many steps to localize the target."
     return localized
 
@@ -132,6 +90,11 @@ def naive_next_pos(measurement, OTHER = None):
 # This is how we create a target bot. Check the robot.py file to understand
 # How the robot class behaves.
 test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
-test_target.set_noise(0.0, 0.0, 0.0)
+measurement_noise = 0.05 * test_target.distance
+test_target.set_noise(0.0, 0.0, measurement_noise)
 
-demo_grading(estimate_next_pos, test_target)
+demo_grading(naive_next_pos, test_target)
+
+
+
+
